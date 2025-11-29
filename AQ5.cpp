ad466556532dd4ff48aacbca3c412f19f0262704
Vaibhav Singhal <vaibhavsinghal0123@gmail.com>
2025-11-30 02:56:50 +0530

    Add files via upload

---
+++ AQ5.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <unordered_set>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next;
+    
+    Node(int val) {
+        data = val;
+        next = NULL;
+    }
+};
+
+bool hasCycle(Node *head) {
+    unordered_set<Node*> visited;
+    
+    Node* temp = head;
+    while (temp != NULL) {
+        if (visited.count(temp)) {
+            return true;
+        }
+        visited.insert(temp);
+        temp = temp->next;
+    }
+    return false;
+}
+
+int main() {
+    Node* n1 = new Node(1);
+    Node* n2 = new Node(2);
+    Node* n3 = new Node(3);
+    Node* n4 = new Node(4);
+    
+    n1->next = n2;
+    n2->next = n3;
+    n3->next = n4;
+    n4->next = n2; 
+    
+    if (hasCycle(n1)) {
+        cout << "true (Loop detected)" << endl;
+    } else {
+        cout << "false" << endl;
+    }
+    
+    return 0;
+}
